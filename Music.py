e471b2140d171fd7ff0f10f38c83277fac378a38
ArashkPA <arashk.asadii@gmail.com>
2025-06-02 00:51:07 +0330

    Add files via upload

--- Music.py
+++ Music.py
@@ -1,7 +1,9 @@
 import sqlite3
-conn = sqlite3.connect('songs.db')
- 
+from flask import Flask
+import os
 
+# ساخت دیتابیس و جدول
+conn = sqlite3.connect('songs.db')
 c = conn.cursor()
 c.execute('''
     CREATE TABLE IF NOT EXISTS songs (
@@ -11,8 +13,18 @@ c.execute('''
         text TEXT
     )
 ''')
-
 conn.commit()
 conn.close()
 
 print("دیتابیس ساخته شد و جدول songs آماده است.")
+
+# ایجاد اپ Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot and Database are running!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
